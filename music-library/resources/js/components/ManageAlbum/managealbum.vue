11c209507e42fd3048acb43359b22ed003e57db1
Khan Abrar Shams <Khan.abrar.shams@g.bracu.ac.bd>
2023-11-18 20:10:26 +0600

    search button album

--- music-library/resources/js/components/ManageAlbum/managealbum.vue
+++ music-library/resources/js/components/ManageAlbum/managealbum.vue
@@ -2,15 +2,19 @@
     <div class="flex w-[640px] sm:w-screen sm:h-screen bg-gradient-to-r from-gray-900 to-gray-700 dark:bg-gradient-to-r dark:from-neutral-900 dark:to-neutral-900">
 
         
-        <div class=" rounded-lg min-w-[20%] sm:min-w-[13%] max-w-[18%] grid pt-2 pb-3 px-3 ">
+        <div class=" rounded-lg min-w-[20%] sm:min-w-[13%] max-w-[18%] grid pt-2 mb-3 px-2 ">
             <!-- mt-[-35%] w-[90%] h-[90%] --> 
             <ManageYourAlbums class="bg-gray-700 shrink h-[100%] sm:h-[93%] overflow-y-auto "></ManageYourAlbums>
         </div>
 
-        <div class="flex bg-blue-300 grow ">
-
-            <div class="bg-cyan-300 min-w-[25%] max-w-[35%]">
-                Your Songs here
+        <div class="flex grow rounded-lg">
+            <div class=" min-w-[25%] max-w-[35%]  grid pt-2 mb-3 sm:px-1  md:px-2 ">
+                <div class="h-[100%] sm:h-[93%] overflow-y-auto  bg-gray-700 dark:bg-neutral-800 rounded-lg">
+                    <search-bar class="flex p-2"> </search-bar>
+                    <div class="flex p-2">
+                        Your Songs here
+                    </div>
+                </div>
             </div>
             
             <div class="bg-green-300 grow">
