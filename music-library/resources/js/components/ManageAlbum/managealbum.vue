b6b26d62a73bda150e4418ad521a49cce68b087a
Khan Abrar Shams <Khan.abrar.shams@g.bracu.ac.bd>
2023-11-19 22:27:19 +0600

    manage upload frontend update

--- music-library/resources/js/components/ManageAlbum/managealbum.vue
+++ music-library/resources/js/components/ManageAlbum/managealbum.vue
@@ -2,13 +2,13 @@
     <div class="flex w-[640px] sm:w-screen sm:h-screen bg-gradient-to-r from-gray-900 to-gray-700 dark:bg-gradient-to-r dark:from-neutral-900 dark:to-neutral-900">
 
         
-        <div class=" rounded-lg min-w-[20%] sm:min-w-[13%] max-w-[18%] grid pt-2 mb-3 px-2 ">
+        <div class=" rounded-lg min-w-[20%] sm:min-w-[13%] max-w-[18%] grid pt-2 mb-3 px-1 ">
             <!-- mt-[-35%] w-[90%] h-[90%] --> 
             <ManageYourAlbums class="bg-gray-700 shrink h-[100%] sm:h-[93%] overflow-y-auto "></ManageYourAlbums>
         </div>
 
-        <div class="flex grow rounded-lg">
-            <div class=" min-w-[25%] max-w-[35%]  grid pt-2 mb-3 sm:px-1  md:px-2 ">
+        <div class="flex grow rounded-lg ">
+            <div class=" min-w-[25%] max-w-[35%]  grid pt-2 mb-3 px-1">
                 <div class="h-[100%] sm:h-[93%] overflow-y-auto  bg-gray-700 dark:bg-neutral-800 rounded-lg">
                     <search-bar class="flex p-2"> </search-bar>
                     <div class="flex p-2">
@@ -17,8 +17,12 @@
                 </div>
             </div>
             
-            <div class="bg-green-300 grow">
-                playlist thingy
+            <div class="flex grow rounded-lg px-1">
+                <div class=" grow grid pt-2 mb-3">
+                    <div class="h-[100%] sm:h-[93%] overflow-y-auto  bg-gray-700 dark:bg-neutral-800 rounded-lg">
+                        Playlist thingy
+                    </div>
+                </div>
             </div>
         </div>
     </div>
